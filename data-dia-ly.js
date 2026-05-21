4e124f6984bb9da4c64d167242fc5fb4795626ab
quoczoe <mynameisanhquocpro@gmail.com>
2026-05-22 05:03:42 +0700

    Create data-dia-ly.js

---
+++ data-dia-ly.js
@@ -0,0 +1,18 @@
+// DỮ LIỆU ĐỊA LÝ DÙNG CHUNG CHO TOÀN HỆ THỐNG
+const KHO_DATA_DIA_LY = {
+    "Nam": {
+        "Hồ Chí Minh": ["Gò Vấp", "Quận 12", "Tân Bình", "Bình Thạnh", "Thủ Đức", "Hóc Môn", "Quận 10", "Quận 11"],
+        "Kiên Giang": ["Rạch Giá", "Phú Quốc", "Hà Tiên", "Châu Thành"],
+        "Cần Thơ": ["Ninh Kiều", "Cái Răng", "Bình Thủy"],
+        "Bình Dương": ["Thủ Dầu Một", "Dĩ An", "Thuận An"]
+    },
+    "Trung": {
+        "Đà Nẵng": ["Hải Châu", "Thanh Khê", "Sơn Trà", "Liên Chiểu"],
+        "Khánh Hòa": ["Nha Trang", "Cam Ranh", "Ninh Hòa"]
+    },
+    "Bac": {
+        "Hà Nội": ["Cầu Giấy", "Đống Đa", "Ba Đình", "Thanh Xuân", "Hà Đông", "Hoàng Mai"],
+        "Thái Bình": ["Thành phố Thái Bình", "Đông Hưng", "Tiền Hải", "Thái Thụy"],
+        "Nam Định": ["Thành phố Nam Định", "Giao Thủy", "Hải Hậu", "Nghĩa Hưng"]
+    }
+};
